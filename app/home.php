d2e42d21d0a88a138f1eedb3b88b0acdbb9b17fe
Ricardo <ricardoacedo7@gmail.com>
2015-01-11 11:34:27 +0100

    calendario home

--- app/home.php
+++ app/home.php
@@ -2,12 +2,87 @@
 session_start();
 if (isset($_SESSION['user'])) {
     include_once './includes/functions.php';
+    conectarBD();
     ?>
     <!DOCTYPE HTML>
     <html>
         <head>
             <title>Social Flat - Inicio</title>
             <?php include_once './includes/headers.html'; ?>
+            <script type="text/javascript">
+                var zonas = [];
+                var nombres = [];
+    <?php
+    if (isset($_SESSION['idpiso'])) {
+        $idpiso = $_SESSION['idpiso'];
+        $result = mysql_query("SELECT * FROM `limpieza` WHERE `ID_piso`='$idpiso'");
+        if (mysql_num_rows($result) > 0) {
+            $datos = mysql_fetch_row($result);
+            $inicio = $datos[1];
+            $fin = $datos[2];
+            $frecuencia = $datos[3];
+            $result = mysql_query("SELECT nombre FROM `zonas_limpieza` WHERE `ID_piso`='$idpiso'");
+            while ($zona = mysql_fetch_row($result)) {
+                echo "zonas[zonas.length]=" . json_encode($zona) . ";";
+            }
+            $result2 = mysql_query("SELECT username FROM `user` WHERE `id_piso`='$idpiso'");
+            while ($nombre = mysql_fetch_row($result2)) {
+                echo "nombres[nombres.length]=" . json_encode($nombre) . ";";
+            }
+            ?>
+                        Date.prototype.addDays = function(days)
+                        {
+                            var dat = new Date(this.valueOf());
+                            dat.setDate(dat.getDate() + parseInt(days));
+                            return dat;
+                        };
+                        var fechaInicio = new Date("<?php echo $inicio; ?>");
+                        //Establecemos la fecha final
+                        var fechaFinal = new Date("<?php echo $fin; ?>");
+                        var frecuencia = <?php echo $frecuencia; ?>;
+                        //Restamos la fechaFinal menos fechaInicio, 
+                        //esto establece la diferencia entre las fechas
+                        var fechaResta = fechaFinal - fechaInicio;
+                        //Transformamos el tiempo de diferencia en días.
+                        fechaResta = (((fechaResta / 1000) / 60) / 60) / 24;
+                        var colores = ['blue', 'green', 'red', 'purple', 'chocolate', 'cyan', 'deeppink', 'gold', 'brown', 'black'];
+                        var n = 0;
+                        var eventos = [];
+                        for (var i = 0; i < fechaResta; i += frecuencia) {
+                            var days = 1000 * 60 * 60 * 24 * i;
+                            var d1 = new Date(fechaInicio.getTime() + days);
+                            var d2 = fechaInicio.addDays(i + frecuencia);
+                            if (nombres.length === zonas.length) {
+                                n++;
+                                if (n === nombres.length) {
+                                    n = 0;
+                                }
+                            }
+                            var nAux = nombres[n];
+                            for (var j = 0; j < zonas.length; j++) {
+                                n++;
+                                if (n === nombres.length) {
+                                    n = 0;
+                                }
+                                var titulo = 'Zona:' + zonas[j] + '. Limpiador: ' + nombres[n];
+                                eventos[eventos.length] = {title: titulo,
+                                    start: d1.getFullYear() + '-' + (parseInt(d1.getMonth()) + 1) + '-' + d1.getDate(),
+                                    end: d2.getFullYear() + '-' + (parseInt(d2.getMonth()) + 1) + '-' + d2.getDate(),
+                                    color: colores[j],
+                                    allDay: true};
+                            }
+                        }
+                        $(document).ready(function() {
+                            $('#calendar').fullCalendar({
+                                events: eventos,
+                                aspectRatio: 1
+                            });
+                        });
+            <?php
+        }
+    }
+    ?>
+            </script>
         </head>
         <body class="right-sidebar" onload="<?php
         if (nuevo()) {
@@ -86,35 +161,12 @@ if (isset($_SESSION['user'])) {
                                     }
                                     echo number_format($total_balance / $divisor, 2);
                                     ?> €</div></a>
-                                <?php ?>
+                            <?php ?>
 
                         </div>
                         <div class="4u" id="sidebar">
                             <hr class="first" />
-                            <section>
-                                <header>
-                                    <h3>&iquest;Conoce las ventajas de ser miembro de Social Flat?</h3>
-                                </header>
-                                <p>
-                                    Ser miembro de social flat es gratuito y no tiene ning&uacute;n
-                                    tipo de compromiso. En la comunidad conocer&aacute; mucha gente
-                                    y &iexcl;podr&aacute; llevar la gesti&oacute;n de su piso de una forma
-                                    m&aacute;s c&oacute;moda!
-                                </p>
-                            </section>
-                            <hr />  
-                            <section>
-                                <header>
-                                    <h3><a href="#">No olvide leer las condiciones de servicio</a></h3>
-                                </header>
-                                <p>
-                                    Son cortas y de obligatoria lectura. &iexcl;Las aceptas automaticamente al registrarte
-                                    en Social Flat!      
-                                </p>
-                                <footer>
-                                    <a class="button" onclick="terms()">Condiciones</a>
-                                </footer>
-                            </section>
+                            <div id="calendar"></div>
                         </div>
                     </div>
                 </div>
